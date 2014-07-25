824910c52160dc8ec462d34304b03ae0ec84dedc
Antriksh Agarwal <AntrikshA@users.noreply.github.com>
2014-07-26 00:12:02 +0530

    Update the_library.c

--- the_library.c
+++ the_library.c
@@ -1,375 +1,194 @@
-//	The LIBRARY !
+//LIBRARY
 
-#include <stdio.h>
-#include <termios.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>   
+#include <stdio.h>
+#include <math.h>
+#include <termios.h>
+#include <unistd.h>
 
-#define SIZE 100
 
-typedef struct book
-{
-	char usrname[20];
-	char book[50];
-	struct book *ptr;
-}b;
+typedef struct password_list{
+	char *username;
+	char *password;
+	struct password_list *ptr;
+}p;
 
-typedef struct usrnm
-{
-	char usr[20];
-	char pass[20];
-	struct usrnm *link;
-}u;
+typedef struct database_list{
+	char *user;
+	char *book;
+	struct database_list *link;
+}d;
 
-b *start;
-u *first;
+p *first;
+d *start;
 
-void fileget()
-{
-	FILE *p;
-	b *a,*t;
-	a=(b*)malloc(sizeof(b));
-	start=NULL;
-	t=start;
-	p=fopen("database.txt","w+");
-	while(feof(p))
-	{
-		fscanf(p,"\t%s\t%s\n",a->usrname,a->book);
-		a->ptr=NULL;
-		if(start==NULL) {
-			a=start;
-		} else {
-			while(t->ptr!=NULL) {
-				t=t->ptr;
-			}
-			t->ptr=a;
-		}
-		printf("\t%a\t%s\n",a->usrname,a->book);
-	}
-	fclose(p);
-}
-
-u* filegetpass()
-{
-	FILE *p;
-	u *a;
-	a=first;
-	p=fopen("password.txt","r+");
+void add(p *a){
+	//printf("hello");
+	p *temp;
+	first->ptr=NULL;
+	//temp=(p*)malloc(sizeof(p));
+	//printf("\n\nIn add");
 	__fpurge(stdin);
-	//fflush(stdin);
-	while(feof(p))
-	{
-		if(first==NULL)
-		{
-			printf("a");
-			fscanf(p,"\t%s\t%s",first->usr,first->pass);
-			first->link=NULL;
-			a=first;
+	if (first->username==NULL && first->password==NULL){
+		first->username=a->username;
+		first->password=a->password;
+		first->ptr=NULL;
+		//printf("\n\n%s : %s\n\n",first->username,first->password);
+	}
+	else{
+		temp=first;
+		printf("\n\t%s : %s\n",temp->username,temp->password);
+		while(temp->ptr!=NULL){
+			printf("\nHELLO WORLD\n");
+			printf("\n\t%s : %s\n",temp->username,temp->password);
+			temp=temp->ptr;
+			
 		}
-		printf("a");
-		fscanf(p,"\t%s\t%s",a->usr,a->pass);
-		a->link=NULL;
-		a=a->link;	
+		temp=a;
+		temp->ptr = NULL;
+		//printf("\n%s :: %s\n",temp->username,temp->password);
 	}
-	printf("Done");
-	a=first;
-	while(a->link!=NULL)
-	{
-		printf("\t%s\t%s\n",a->usr,a->pass);
-		a=a->link;
-	}
-	fclose(p);
-	return first;
-}
-
-void getPassword(char password[])
-{
-    static struct termios oldt, newt;
-    int i = 0;
-    int c;
-    __fpurge(stdin);
-    //fflush(stdin);
-    /*saving the old settings of STDIN_FILENO and copy settings for resetting*/
-    tcgetattr( STDIN_FILENO, &oldt);
-    newt = oldt;
-    /*setting the approriate bit in the termios struct*/
-    newt.c_lflag &= ~(ECHO);          
-    /*setting the new bits*/
-    tcsetattr( STDIN_FILENO, TCSANOW, &newt);
-    /*reading the password from the console*/
-    fflush(stdin);
-    while ((c = getchar())!= '\n' && c != EOF && i < SIZE){
-        password[i++] = c;
-    }
-   	password[i] = '\0';
-    /* resetting our old STDIN_FILENO */ 
-    tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
-    return;
-}
-
-u* check()
-{
-	u *a, *t;
-	char ch, copy[20];
-	a=(u*)malloc(sizeof(u));
-	t=(u*)malloc(sizeof(u));
-	int i;
-	t=first;
-	__fpurge(stdin);
-	//fflush(stdin);
-	if(first==NULL)
-	{		
-		printf("\n\t::No users entered yet::\n\n");
-		printf("Sign Up? (y/n): ");
-		__fpurge(stdin);
-		//fflush(stdin);
-		scanf("%c",&ch);
-		if(ch=='y')
-			user();
-		else
-		{
-			exit(0);
-		}
-		check();
-	}
-	else
-	{
-		i=0;
-		printf("Username: ");
-		scanf("%d",a->usr);
-		printf("Password: ");
-		getPassword(a->pass);
-		while(t!=NULL)
-		{
-			if(t->usr==a->usr && t->pass==a->pass)
-			{
-				printf("\n::Welcome %s::\n",a->usr);
-				return a;
-			}
-			t=t->link;
-		}
-		printf("\n::Wrong Username OR Password::\n");
-		return a;
-	}
-}
-
-b* addbk()
-{
-	b *a, *t, *s;
-	u *z;
-	FILE *file;
-	file=fopen("database.txt","w");
-	char copy[50];
-	a=(b*)malloc(sizeof(b));
-	t=(b*)malloc(sizeof(b));
-	z=(u*)malloc(sizeof(u));
-	t=start;
-	a=NULL;
-	z = check();
-	__fpurge(stdin);
-	//fflush(stdin);
-	strcpy(a->usrname,z->usr);
-	if(a->usrname!=NULL)
-	{
-		__fpurge(stdin);
-		//fflush(stdin);
-		printf("Enter your book: ");
-		scanf("%s",a->book);
-		while(t->ptr!=NULL)
-		{
-			if(t==start)
-			{
-				start=a;
-				start->ptr=NULL;
-			}
-			else if(t==start->ptr)
-			{
-				if(start->ptr==t->ptr)
-				{
-					start->ptr=a;
-					a->ptr=t;
-					t->ptr=NULL;
-				}
-				else
-				{
-					t->ptr=a;
-					t->ptr->ptr=NULL;
-				}
-			}
-			else
-			{
-				if(strcmp(a->usrname,t->usrname)==0 && strcmp(a->usrname,t->ptr->usrname)>0)
-				{
-					a->ptr=t->ptr;
-					t->ptr=a;
-				}
-				else if(strcmp(a->usrname, t->usrname)>0 && strcmp(a->usrname, t->ptr->usrname)<0)
-				{
-					a->ptr=t->ptr;
-					t->ptr=a;
-				}
-			}
-			fprintf(file,"\t%s\t%s\n",t->usrname,t->book);
-			t=t->ptr;
-		}
-	}
-	else
-	{
-		printf("\n\tUser making failed !\n");
-	}
-	fclose(file);
+	//writefile();
 	return;
 }
 
-b* rembk()
-{
-	b *a, *t;
-	u *z;
-	FILE *file;
-	a=(b*)malloc(sizeof(b));
-	t=(b*)malloc(sizeof(b));
-	z=(u*)malloc(sizeof(u));
-	t=start;
-	a=NULL;
-	file=fopen("database.txt","w");
-	z=check();
+writefile(){
+	FILE *f;
+	char *s;
+	int i;
+	p *a;
+	printf("\nIn writefile\n");
+	s=(char*)malloc(sizeof(char)*50);
+	//s=NULL;
+	a=(p*)malloc(sizeof(p));
+	f=fopen("password1.txt","w");
+	printf("hello");
+	a=first;
+	while(a->ptr!=NULL){
+		//fprintf(f,"%s\t%s",a->username,a->password);
+		printf("%s\t%s",a->username,a->password);
+		a=a->ptr;
+	}
+	fclose(f);
+}
+
+void openfile(){
+	FILE *f;
+	char *s1,*s2;
+	int i;
+	p *a;
+	s1=(char*)malloc(sizeof(char)*50);
+	s2=(char*)malloc(sizeof(char)*50);
+	//s1=NULL;
+	a=(p*)malloc(sizeof(p));
+	f=fopen("password.txt","r");
 	__fpurge(stdin);
-	//fflush(stdin);
-	strcpy(a->usrname,z->usr);
-	if(a->usrname!=NULL)
-	{
-		printf("Enter book to be removed: ");
-		scanf("%s",a->book);
-		while(t!=NULL)
+	while(!feof(f)){
+		//printf("Not printing here");
+		fscanf(f,"%s\t%s",s1,s2);
+		if (s1==NULL)
 		{
-			if(strcmp(a->usrname,t->ptr->usrname)==0 && strcmp(a->book, t->ptr->book)==0)
-			{
-				printf("Removed: %s from user %s",t->ptr->book,t->ptr->usrname);
-				t->ptr=t->ptr->ptr;
+			printf("\nNo user data Exists... \n\n");
+		}
+		a->username=s1;
+		//printf("%s\n",a->username);
+		//fscanf(f,"%s",s);
+		a->password=s2;
+		//printf("%s\n",a->username);
+		//printf("%s\n",a->password);
+		add(a);
+	}
+	fclose(f);
+}
+
+int check(char s[], char *pass){
+	char c;
+	int flag=0;
+	p *a;
+	openfile();
+	a=first;
+	while(a->ptr!=NULL){
+		printf("\n\n%s\t%s\n\n",a->username,a->password);
+		if(a->username==s && a->password==pass){
+			flag=1;
+		}
+		a=a->ptr;
+	}
+	if (flag==0){
+		printf("You have not been signed up... Wanna sign up ?(Y/n) : ");
+		scanf("%c",&c);
+		if(c=='Y'){
+			if(signup(0)==-1){
+				return -1;
 			}
-			fprintf(file,"\t%s\t%s\n",t->usrname,t->book);
-			t=t->ptr;
+		}
+		else{
+			return -1;
 		}
 	}
-	fclose(file);
 }
 
-b* searchbk()
-{
-	b *t;
-	char a[50];
-	t=(b*)malloc(sizeof(b));
-	t=start;
+int signup(int n){
+	char *s;
+	p *a;
+	char *pass;
+	char *repass;
+	a=(p*)malloc(sizeof(p));
 	__fpurge(stdin);
-	//fflush(stdin);
-	printf("Enter book to be searched: ");
-	scanf("%s",a);
-	while(t->ptr!=NULL)
-	{
-		if(strcmp(a,t->ptr->book)==0)
-		{
-			printf("Book: %s with %s",t->ptr->book,t->ptr->usrname);
-		}
-		t=t->ptr;
+	printf("Enter Username : ");
+	scanf("%s",s);
+	//printf("Enter Password : ");
+	pass = getpass("Enter Password : ");
+	repass = getpass("Re-enter Password : ");
+	if(strcmp(pass,repass)==0){
+		a->username=s;
+		a->password=repass;
+		add(a);
 	}
-	return 0;
+	else if(n<3){
+		printf("Passwords do not match...\n\n");
+		signup(n+1);
+		//return -1;
+	}
+	else{
+		return -1;
+	}
+	return;
 }
 
-void display()
-{
-	b *t;
-	t=start;
+int database(){
+	printf("Database here");
+}
+
+int login(){
+	char s[50];
+	char *pass;
 	__fpurge(stdin);
-	//fflush(stdin);
-	printf("\tUsername\tBook\n");
-	while(t!=NULL)
-	{
-		printf("\t%s\t%s\n",t->usrname, t->book);
-		t=t->ptr;
+	printf("Enter Username : ");
+	fgets(s,50, stdin);
+	pass = getpass("Enter Password : ");
+	//printf("%s\n",s);
+	//printf("%s\n\n",pass);
+	if (check(s,pass)==-1){
+		return -1;
 	}
 }
 
-int user()
-{
-	FILE *file;
-	u *a, *t;
-	char passwd[50];
-	__fpurge(stdin);
-	//fflush(stdin);
-	a=(u*)malloc(sizeof(u));
-	t=(u*)malloc(sizeof(u));	
-	file=fopen("password.txt","a");
-	printf("Enter Username: ");
-	scanf("%s",a->usr);
-	printf("Enter Password: ");
-	getPassword(a->pass);
-	printf("\nConfirm Password: ");
-	getPassword(passwd);
-	first=filegetpass();
-	if(strcmp(a->pass,passwd)!=0)
-	{
-		printf("\n\nPasswords Do Not Match!\n\n");
-		user();
+void main(){
+	first=(p*)malloc(sizeof(p));
+	first->username=NULL;
+	first->password=NULL;
+	start=(d*)malloc(sizeof(d));
+	start->user=NULL;
+	start->book=NULL;
+	printf("\n\n\t\tWelcome to the Library !\n\n");	
+	if(login()==-1){
+		printf("\n\n\t   THANK YOU for using the software...\n\n\t\t\tBYE-BYE !\n\n");
+		return;
 	}
-	else 
-	{
-		printf("\n\n\t::Welcome %s::\n\n",a->usr);
-		if(first==NULL)
-		{
-			first=a;
-			first->link=NULL;
-			fprintf(file,"\t%s\t%s\n",first->usr,first->pass);
-			printf("\t::Succesfully Added::\n");
-		}
-		else
-		{
-			t=first;
-			while(t->link!=NULL)
-			{t=t->link;}
-			t->link=a;
-			t->link=NULL;
-			fprintf(file,"\t%s\t%s\n",t->usr,t->pass);
-			printf("\t::Succesfully Added Again::\n");
-		}
+	if(database()==-1){
+		printf("\n\n\t   THANK YOU for using the software...\n\n\t\t\tBYE-BYE !\n\n");
+		return;
 	}
-	fclose(file);
-	return 0;
-}
-
-void main ()
-{
-	int n;
-	first=(u*)malloc(sizeof(u));
-	start=(b*)malloc(sizeof(b));
-	first=NULL;
-	while(1)
-	{
-		printf("\n1. Add book\n2. Remove a book\n3. Search for a book\n4. New User\n5. Display Users with their Books.\n6. Exit\n");
-		printf("\nEnter Your Choice: ");
-		scanf("%d",&n);
-		switch(n)
-		{
-			case 1:
-				addbk();
-				break;
-			case 2:
-				rembk();
-				break;
-			case 3:
-				searchbk();
-				break;
-			case 4:
-				user();
-				break;
-			case 5:
-				display();
-				break;
-			case 6:
-				exit(0);
-				break;
-			default:
-				printf("\n\nI am sorry... No action to be performed at this input!\n");
-		}
-	}
+	return;
 }
